7db4dcb5ddea0cce59adedcdf5bbe62633b45aa8
sumit <sumitdhole8474@gmail.com>
2026-02-24 14:34:03 +0530

    gogle

--- src/app/components/Reviewrow.tsx
+++ src/app/components/Reviewrow.tsx
@@ -103,13 +103,31 @@ export default function ReviewRow({
           {review.review}
         </p>
 
-        {/* ✅ GOOGLE ICON (Bottom Left) */}
+        {/* ✅ GOOGLE "G" ICON - Bottom Left (SVG) */}
         <div className="absolute bottom-4 left-4">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg"
-            alt="Google"
-            className="w-6 h-6 object-contain"
-          />
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 48 48"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#EA4335"
+              d="M24 9.5c3.54 0 6.71 1.22 9.2 3.6l6.9-6.9C35.5 2.4 30.2 0 24 0 14.7 0 6.7 5.5 2.8 13.5l8.1 6.3C13 13 18 9.5 24 9.5z"
+            />
+            <path
+              fill="#4285F4"
+              d="M46.1 24.5c0-1.7-.1-3.3-.4-4.8H24v9.1h12.4c-.5 2.7-2 5-4.2 6.6l6.5 5C42.8 36.5 46.1 31 46.1 24.5z"
+            />
+            <path
+              fill="#FBBC05"
+              d="M10.9 28.8c-.5-1.4-.8-2.9-.8-4.3s.3-2.9.8-4.3l-8.1-6.3C1 17.3 0 20.6 0 24s1 6.7 2.8 10.1l8.1-6.3z"
+            />
+            <path
+              fill="#34A853"
+              d="M24 48c6.5 0 12-2.1 16-5.7l-6.5-5c-2 1.4-4.6 2.2-9.5 2.2-6 0-11-3.5-13.1-8.5l-8.1 6.3C6.7 42.5 14.7 48 24 48z"
+            />
+          </svg>
         </div>
 
         {/* ================= ACTION BUTTONS ================= */}
